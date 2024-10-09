9f4b3110be58857527082560047dcc4b4ed79865
Fouad Adnan <fouadadnan07@gmail.com>
2024-10-09 09:14:04 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+
+let selecta = document.getElementById('selecta')
+let genre
+
+function handlechangeSelect(){
+    genre = selecta.value
+    fetch(`https://api.api-ninjas.com/v1/${genre}?X-Api-Key=W2JzAVUPGhsGmIsJWLiVNUFy763tpv8ru00jRS6T`)
+.then(Response => Response.json())
+.then(Response => DisplayQuotes(Response[0]))
+}
+let author = document.getElementById('author')         
+
+function DisplayQuotes(Response){
+    let quot = document.getElementById('quot')
+    // genre == 'quotes' ? quot.innerHTML = Response.quote  : quot.innerHTML = Response.joke 
+    // genre == 'jokes' ? quot.innerHTML = Response.joke   :  quot.innerHTML = Response.quote
+    if(genre == 'jokes'){
+        quot.innerHTML = Response.joke 
+        author.innerText = ''
+
+    }else if(genre == 'facts'){
+        quot.innerHTML = Response.fact
+        author.innerText = ''
+        
+    }else{
+        quot.innerHTML = Response.quote
+        author.innerHTML = Response.author
+
+    }
+
+
+   
+}
